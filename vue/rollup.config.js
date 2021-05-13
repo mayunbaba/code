28fa3cdea59897cd395244890645376538cc6422
liuweihua <18538302307@163.com>
2021-05-13 16:15:51 +0800

    feat: 性能监控初始化

--- vue/rollup.config.js
+++ vue/rollup.config.js
@@ -1,18 +1,27 @@
-import babel from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel' // rollup 的 babel 插件，ES6转ES5
 import serve from 'rollup-plugin-serve'
+import nodeResolve from 'rollup-plugin-node-resolve' // 帮助寻找node_modules里的包
+import commonjs from 'rollup-plugin-commonjs' // 将非ES6语法的包转为ES6可用
 
 export default {
   input: './src/index.js', // 入口文件
+  // 作用：指出应将哪些模块视为外部模块，否则会被打包进最终的代码里
+  external: ['htmlparser2'],
   output: {
     format: 'umd', // 模块化类型 esModule commonjs模块
     name: 'Vue', // 全局变量的名字
     file: 'dist/umd/vue.js',
-    sourcemap: true
+    sourcemap: true,
+    globals: {
+      htmlparser2: 'htmlparser2', // 这跟external 是配套使用的
+    }
   },
   plugins: [
+    nodeResolve(),
     babel({
       exclude: 'node_modules/**'
     }),
+    commonjs(),
     serve({
       open: true,
       port: 3000,
--- vue/src/compiler/index.js
+++ vue/src/compiler/index.js
@@ -1,3 +1,40 @@
-export function compileToFunction() {
-  
+// const htmlparser2 = require("htmlparser2")
+import htmlparser2 from 'htmlparser2'
+console.log(htmlparser2)
+const parser = new htmlparser2.Parser({
+  onopentag(name, attributes) {
+    console.log(name, attributes)
+  },
+  ontext(text) {
+    console.log(text)
+  },
+  onclosetag(tagname) {
+    console.log(tagname)
+  }
+})
+
+function parserHTML(html) {
+  function advance(len) {
+    html = html.substring(len)
+  }
+  // parser.write(html)
+  // while(html) { // 看解析的内容是否存在，如果存在就不停地解析
+  //   let textEnd = html.indexOf('<') // 当前解析的开头  
+  //   if (textEnd === 0) {
+  //     const startTagMatch = parseStartTag(html) // 解析开始标签
+  //     break;
+  //     if (startTagMatch) {
+
+  //     }
+  //     const endTagMatch = parseEndTag(html)
+  //     if (endTagMatch) {
+
+  //     }
+  //   }
+  // }
+}
+
+// html 字符串解析成对应的脚本来触发 tokens <div id="app">{{name}}</div>
+export function compileToFunction(template) {
+  parserHTML(template)
 }
