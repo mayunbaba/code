e86d4633e244b723767bd653540453be7780b1bc
liuweihua <18538302307@163.com>
2021-07-13 16:45:54 +0800

    抽离数据处理

--- xueqiuData/src/convertibleBond/index.js
+++ xueqiuData/src/convertibleBond/index.js
@@ -6,48 +6,65 @@ const { fileDisplay, sortBy, writeXlsx } = require('../utils')
 
 
 const wencaiFile = fileDisplay(path.resolve(__dirname, './wencai/'))
-const latestData = sortBy(wencaiFile, 'name', -1)[0]
-const formData = xlsx.parse(latestData.path)[0].data
-formData.shift()
-// 到期日 升序 数值越小越好
-// 溢价率 升序 数字越小越好
-// 到期收益率 降序 述职越大越好
-// 排序字典
-const keyMap = {
-  '到期日': {
-    index: 6,
-    order: 1,
-  },
-  '溢价率': {
-    index: 7,
-    order: 1,
-  },
-  '到期收益率': {
-    index: 8,
-    order: -1
-  },
-}
-// 添加排序字段
-formData.map(item => item.push(0))
-// 获取单条数据长度
-const len = formData[0].length
+sortBy(wencaiFile, 'name', -1)
+const latestData = wencaiFile[0]
+let formData = xlsx.parse(latestData.path)[0].data
+sortConvertibleBond(formData)
 
-Object.keys(keyMap).forEach(key => {
-  const item = keyMap[key]
-  sortBy(formData, item.index, item.order ).map((item, index) => {
-    item[len - 1] += index
-  });
-})
-// 按照最后一项升序排序
-sortBy(formData, len - 1 )
-// 取前20项
-formData.splice(20, formData.length - 1)
+
+if (wencaiFile[1]) {
+  // const oldData = wencaiFile[1]
+  // const oldFormData = xlsx.parse(oldData.path)[0].data
+  // console.log(oldFormData)
+  const wencaiFile = fileDisplay(path.resolve(__dirname, './wencai/'))
+  sortBy(wencaiFile, 'name', -1)
+  const latestData = wencaiFile[0]
+  const formData = xlsx.parse(latestData.path)[0].data
+}
 
 writeXlsx(
   latestData.name,
   path.resolve(__dirname, `./resFile/${latestData.name}`),
   formData
-  )
+)
+
+function sortConvertibleBond(formData) {
+  formData.shift()
+  // 到期日 升序 数值越小越好
+  // 溢价率 升序 数字越小越好
+  // 到期收益率 降序 述职越大越好
+  // 排序字典
+  const keyMap = {
+    '到期日': {
+      index: 6,
+      order: 1,
+    },
+    '溢价率': {
+      index: 7,
+      order: 1,
+    },
+    '到期收益率': {
+      index: 8,
+      order: -1
+    },
+  }
+  // 添加排序字段
+  formData.map(item => item.push(0))
+  // 获取单条数据长度
+  const len = formData[0].length
+
+  Object.keys(keyMap).forEach(key => {
+    const item = keyMap[key]
+    sortBy(formData, item.index, item.order).map((item, index) => {
+      item[len - 1] += index
+    });
+  })
+  // 按照最后一项升序排序
+  sortBy(formData, len - 1)
+  // 取前20项
+  formData.splice(20, formData.length - 1)
+  return formData
+}
 
 
 
