0c32140f76b0ebb0541433dc5a615690fdc2ba49
liuweihua <18538302307@163.com>
2021-09-29 16:02:40 +0800

    js错误上报完结

--- monitor/src/monitor/utils/tracker.js
+++ monitor/src/monitor/utils/tracker.js
@@ -26,8 +26,8 @@ class SendTracker {
     for (let key in log) {
       if (typeof log[key] === 'number') {
         log[key] = String(log[key])
-      }else if (typeof(obj[i]) == 'object') {
-        newObj[i] = JSON.stringify(obj[i])
+      }else if (typeof log[key] === 'object') {
+        log[key] = JSON.stringify(log[key])
       }
     }
     const body = JSON.stringify({
@@ -37,12 +37,6 @@ class SendTracker {
     this.xhr.setRequestHeader('Content-Type', 'application/json') // 请求体类型
     this.xhr.setRequestHeader('x-log-apiversion', '0.6.0') // 版本号
     this.xhr.setRequestHeader('x-log-bodyrawsize', body.length) // 请求体的大小
-    this.xhr.onload = function() {
-      // console.log(this.xhr.response)
-    }
-    this.xhr.onerror = function(error) {
-      // console.log(error)
-    }
     this.xhr.send(body)
   }
 }
