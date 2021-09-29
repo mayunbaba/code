a806b0e5d8376f48dc06dec44b3f95a58b842b49
liuweihua <18538302307@163.com>
2021-09-29 13:40:19 +0800

    捕获报错

---
+++ monitor/src/monitor/lib/jsError.js
@@ -0,0 +1,26 @@
+import getLastEvent from "../utils/getLastEvent"
+import getSelector from '../utils/getSelector'
+
+export function injectJsError() {
+  // 监听全局未捕获错误
+  window.addEventListener('error', event => {
+    let lastEvent = getLastEvent() // 最后一个交互事件
+    console.log(lastEvent)
+    let log = {
+      kind: 'stability', // 键控指标的大类
+      type: 'error', // 小类型 这是一个错误
+      errorType: 'jsError', // JS执行错误
+      message: event.message, // 报错信息
+      filename: event.filename, // 哪个文件报错
+      position: `${event.lineno}:${event.colno}`, // 报错位置
+      stack: getLines(event.error.stack),
+      selector: lastEvent ? getSelector(lastEvent.path) : '', // 代表最后一个操作的元素
+    }
+    console.log(log)
+  })
+}
+
+function getLines(stack) {
+  // ^开始 \s空白 \s+任意空白符
+  return stack.split('\n').slice(1).map(item => item.replace(/^\s+at\s+/g, '')).join('^')
+}
---
+++ monitor/src/monitor/utils/getSelector.js
@@ -0,0 +1,20 @@
+function getSelectors(path) {
+  return path.reverse().filter(element => {
+    return element !== document && element !== window
+  }).map(element => {
+    if (element.id) {
+      return `${element.nodeName.toLowerCase()}#${element.id}`
+    } else if (element.className && typeof element.className === 'string') {
+      return `${element.nodeName.toLowerCase()}#${element.className}`
+    } else {
+      return element.nodeName.toLowerCase()
+    }
+  }).join(' ')
+}
+
+
+export default function(path) {
+  if (Array.isArray(path)) {
+    return getSelectors(path)
+  }
+}
